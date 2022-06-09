6be35270ca57b433059a76b155ea73a09d83434d
Alaric <alaric@arcanazero.com>
2022-06-09 01:28:30 -0600

    Added a header and footer.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-#[macro_use]
-extern crate rocket;
+use rocket::fs::{relative, FileServer};
+use rocket::*;
+use rocket_dyn_templates::{context, Template};
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    Template::render("index", context! {})
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/", FileServer::from(relative!("static")))
+        .attach(Template::fairing())
 }
